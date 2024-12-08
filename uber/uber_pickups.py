daad9f9b4fac4252037aa2d5dac2425fed88524e
jainee-121 <jainee1215.ahm@gmail.com>
2024-12-08 22:16:25 +0530

    added pages

--- uber/uber_pickups.py
+++ uber/uber_pickups.py
@@ -9,21 +9,31 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase,axis=1, inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+b=st.selectbox("Select",("None","DATA URL","Upload file"))
+file=st.sidebar.file_uploader("choose a file to upload",type="csv")
+data=st.empty()
+@st.cache_resource
+def load_data():
+    if b=="DATA URL":
+        data = pd.read_csv(DATA_URL, nrows=1000)
+        lowercase = lambda x: str(x).lower()
+        data.rename(lowercase,axis=1, inplace=True)
+        data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+        return data
+    if b=="Upload file":
+        data=pd.read_csv(file)
+        return data
+    return st.write("select something nah!!")
 
+
+st.sidebar.header("View Raw Data")
 
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
+data = load_data()
 # Notify the reader that the data was successfully loaded.
-data_load_state.text(" View Raw Data")   
-c=st.selectbox("Select Date",("ALL",40.74,40.75,40.695))
+data_load_state.text("View Raw Data")   
+c=st.selectbox("Select Lat",("ALL",40.74,40.75,40.695))
 if c=="ALL":
     st.write(data) 
 else:
